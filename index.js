ec3c07ae582441fa6825d32041b061ab1cac2b40
principal Kelvo <kelvinnjoroowino@gmail.com>
2022-06-13 15:25:51 +0300

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 // Your code goes here
 document.addEventListener("DOMContentLoaded", function() {
-    console.log("The DOM has loaded first");
     document.querySelector('p').textContent = "This is really cool!";
   });
